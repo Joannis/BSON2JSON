d8dda5225d2c002ffaeb00baa06a5e422eea342e
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-25 22:24:57 +0100

    renaming... slightly

---
+++ Tests/BSONJSON/BSONJSONTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import BSONJSON
+
+class BSONJSONTests: XCTestCase {
+
+	func testExample() {
+		// This is an example of a functional test case.
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
+
+}
+extension BSONJSONTests {
+	static var allTests : [(String, BSON-JSONTests -> () throws -> Void)] {
+		return [
+			("testExample", testExample),
+		]
+	}
+}
