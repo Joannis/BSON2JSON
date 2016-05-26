483831db507ead22c6661ce9ee6d213a97bcd6f9
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-26 20:38:56 +0100

    First implementation

--- Sources/BSON2JSON.swift
+++ Sources/BSON2JSON.swift
@@ -1,3 +1,120 @@
-struct BSON2JSON {
+import enum C7.JSON
+import BSON
+import Foundation
 
+extension BSON.Value {
+    
+    /// This is a "best-effort" conversion, this is *not* Extended JSON spec,
+    /// only an approximation of what makes sense to me.
+    /// See https://docs.mongodb.com/v3.0/reference/mongodb-extended-json/
+    /// for more details, although this code doesn't exactly follow it fully.
+    /// Basically we try to convert the types that are mirrored in JSON natively,
+    /// the rest we wrap in a separate JSON dictionary (Extended-JSON-style)
+    public func toJSON() -> JSON {
+        switch self {
+        case .array(let arrayDoc):
+            return .array(arrayDoc.arrayValue.map { $0.toJSON() })
+        case .binary(let subtype, let data):
+            return binaryToJSON(subtype, data)
+        case .boolean(let bool):
+            return .boolean(bool)
+        case .dateTime(let date):
+            return date.toJSON()
+        case .document(let doc):
+            return docToJSON(doc)
+        case .double(let dbl):
+            return .number(.double(dbl))
+        case .int32(let int):
+            return .number(.integer(Int(int)))
+        case .int64(let int):
+            return .number(.integer(Int(int)))
+        case .javascriptCode(let js):
+            return .string(js)
+        case .javascriptCodeWithScope(let code, let scope):
+            return jsToJSON(code, scope)
+        case .maxKey:
+            return .object(["$maxKey": .number(.integer(1))])
+        case .minKey:
+            return .object(["$minKey": .number(.integer(1))])
+        case .nothing, .null:
+            return .null
+        case .objectId(let oid):
+            return .object(["$oid": .string(oid.hexString)])
+        case .regularExpression(let pattern, let options):
+            return .object(["$regex": .string(pattern), "$options": .string(options)])
+        case .string(let str):
+            return .string(str)
+        case .timestamp(let timestamp):
+            return .string(String(timestamp))
+        }
+    }
+}
+
+// TODO: once this (https://github.com/open-swift/C7/pull/41) is merged, remove it from here
+extension JSON.Number: Equatable { }
+public func ==(lhs: JSON.Number, rhs: JSON.Number) -> Bool {
+    switch (lhs, rhs) {
+    case (.integer(let l), .integer(let r)): return l == r
+    case (.unsignedInteger(let l), .unsignedInteger(let r)): return l == r
+    case (.double(let l), .double(let r)): return l == r
+    default: return false
+    }
+}
+
+extension JSON: Equatable { }
+public func ==(lhs: JSON, rhs: JSON) -> Bool {
+    switch (lhs, rhs) {
+    case (.null, .null): return true
+    case (.boolean(let l), .boolean(let r)): return l == r
+    case (.string(let l), .string(let r)): return l == r
+    case (.number(let l), .number(let r)): return l == r
+    case (.array(let l), .array(let r)): return l == r
+    case (.object(let l), .object(let r)): return l == r
+    default: return false
+    }
+}
+
+//MARK: Document
+extension String {
+    func dropZeroTerminator() -> String {
+        guard let last = self.characters.last where last == "\0" else { return self }
+        return String(self.characters.dropLast())
+    }
+}
+
+func docToJSON(_ doc: Document) -> JSON {
+    var out: [String: JSON] = [:]
+    for (key, value) in doc {
+        out[key.dropZeroTerminator()] = value.makeBsonValue().toJSON()
+    }
+    return .object(out)
+}
+
+//MARK: Javascript
+func jsToJSON(_ code: String, _ scope: Document) -> JSON {
+    return .object(["code": .string(code), "scope": scope.makeBsonValue().toJSON()])
+}
+
+//MARK: binaryData
+func binaryToJSON(_ subtype: BinarySubtype, _ data: [UInt8]) -> JSON {
+    let base64 = NSData(bytes: data, length: data.count).base64EncodedString([])
+    let binType = String(format:"%2X", subtype.rawValue)
+    return .object(["$binary": .string(base64), "$type": .string(binType)])
+}
+
+//MARK: dateTime
+
+private var dateFormatter: NSDateFormatter = {
+    let dateFormatter = NSDateFormatter()
+    let enUSPosixLocale = NSLocale(localeIdentifier: "en_US_POSIX")
+    dateFormatter.locale = enUSPosixLocale
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    return dateFormatter
+}()
+
+extension NSDate {
+    func toJSON() -> JSON {
+        let iso8601String = dateFormatter.string(from: self)
+        return .object(["$date": .string(iso8601String)])
+    }
 }
--- Tests/BSON2JSON/BSONJSONTests.swift
+++ Tests/BSON2JSON/BSONJSONTests.swift
@@ -1,18 +1,66 @@
 import XCTest
 @testable import BSON2JSON
+import BSON
+import enum C7.JSON
 
 class BSON2JSONTests: XCTestCase {
 
-	func testExample() {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
-	}
+    func json(doc: Document) -> JSON {
+        let bson = doc.makeBsonValue()
+        let json = bson.toJSON()
+        return json
+    }
+    
+    func assert(_ doc: Document, _ expected: JSON, file: StaticString = #file, line: UInt = #line) {
+        let gen = doc.makeBsonValue().toJSON()
+        XCTAssertEqual(gen, expected, file: file, line: line)
+    }
+    
+    func testNull() {
+        let exp: JSON = .object(["hello": .null])
+        assert(["hello": .null], exp)
+    }
+    
+    func testDouble() {
+        let exp: JSON = .object(["hello": .number(.double(-43.01))])
+        assert(["hello": .double(-43.01)], exp)
+    }
+    
+    func testInt() {
+        let exp: JSON = .object(["hello": .number(.integer(42))])
+        assert(["hello": .int64(42)], exp)
+    }
 
+	func testString() {
+        let exp: JSON = .object(["hello": .string("world")])
+        assert(["hello": "world"], exp)
+    }
+    
+    func testBoolean() {
+        let exp: JSON = .object(["hello": .boolean(false)])
+        assert(["hello": .boolean(false)], exp)
+    }
+    
+    func testArray() {
+        let exp: JSON = .object(["hello": .array([.string("Earth"), .string("Mars")])])
+        assert(["hello": ["Earth", "Mars"]], exp)
+    }
+    
+    func testObject() {
+        let exp: JSON = .object(["hello": .object(["home": .string("Earth"), "vacation": .string("Mars")])])
+        assert(["hello": ["home": "Earth", "vacation": "Mars"]], exp)
+    }
 }
 extension BSON2JSONTests {
 	static var allTests: [(String, (BSON2JSONTests) -> () throws -> Void)] {
 		return [
-			("testExample", testExample),
-		]
+            ("testNull", testNull),
+            ("testDouble", testDouble),
+            ("testInt", testInt),
+            ("testString", testString),
+            ("testBoolean", testBoolean),
+            ("testArray", testArray),
+            ("testObject", testObject)
+        ]
 	}
 }
